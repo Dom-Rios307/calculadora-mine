b4a2f841e57129df89b37f860bba70bdbc224801
RODRIGO RIOS <riosdom307@gmail.com>
2026-03-10 08:12:47 -0300

    Add files via upload

---
+++ menu_do_mine.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "=== Calculadora do Mine ==="
+
+echo "Digite o primeiro número:"
+read n1
+
+echo "Digite o segundo número"
+read n2
+
+resultado=$ ((n1 + n2))
+
+echo "Resultado da soma:"
+echo $resultado
+
